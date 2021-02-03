58e28b6b3d6c53f4351081857359400f13ae1044
Nagy Dávid <nagyda94@gmail.com>
2021-02-03 12:12:15 +0100

    Putting AIController in place

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,5 +4,20 @@
 #include "TankPlayerController.h"
 
 ATank* ATankPlayerController::GetControlledTank() const {
-	return Cast<ATank>(GetPawn());
+	return Cast<ATank>(GetPawn());	
 } 
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	
+	auto GetTank = GetControlledTank();
+	if (!GetTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController does not work"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Controlled Tank is %s"), *(GetTank->GetName()));
+	}
+	
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController BeginPlay"));
+}
