8623a0f64af40235cc16f8e7eec59a4c335c928b
Nagy Dávid <nagyda94@gmail.com>
2021-02-04 11:31:53 +0100

    GetBarrelLocation

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -3,12 +3,17 @@
 
 #include "Tank.h"
 
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet){
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
+}
+
 // Sets default values
 ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -33,7 +38,7 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 }
 
 void ATank::AimAt(FVector HitLocation) {
-	auto TankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *TankName, *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
+
 }
 
