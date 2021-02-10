c100312978d95bd184b19277207e5730d5ca3250
Nagy Dávid <nagyda94@gmail.com>
2021-02-10 17:00:01 +0100

    MoveForward with W, Better component architecture

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,9 +2,17 @@
 
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
+
+void UTankMovementComponent::Initialize(UTankTrack *LeftToSet, UTankTrack* RightToSet) {
+	if (!LeftToSet || !RightToSet) { return; }
+	LeftTrack = LeftToSet;
+	RightTrack = RightToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
 	UE_LOG(LogTemp, Warning, TEXT("IntendMoveForward: %f"), Throw);
-
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,10 +6,11 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
 /**
- * 
+ * Responsible for driving the tank tracks
  */
-UCLASS()
+UCLASS( ClassGroup = (Custom), meta = (BlueprintSpawnableComponent) )
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
@@ -17,5 +18,14 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void Initialize(UTankTrack* LeftToSet, UTankTrack* RightToSet);
+
+private:
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 	
+
+
 };
